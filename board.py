cdc72bb93847016b195cfca5be01723f3b280b87
CherSu <chersu1126.en09@nycu.edu.tw>
2023-06-06 21:17:46 +0800

    Co-authored-by: yitangyt <yitangyt@users.noreply.github.com>

--- board.py
+++ board.py
@@ -1,6 +1,7 @@
 # 0 = empty black rectangle, 1 = dot, 2 = big dot, 3 = vertical line,
 # 4 = horizontal line, 5 = top right, 6 = top left, 7 = bot left, 8 = bot right
 # 9 = gate
+
 boards = [
 [6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5],
 [3, 6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 3],
@@ -36,3 +37,12 @@ boards = [
 [3, 7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8, 3],
 [7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8]
          ]
+print(len(boards))
+print(len(boards[0]))
+
+"""
+each unit of block size:
+x: 900/len(board[0]) == 900//30
+y: 900//len(board)+2 == 900//33
+x ~= y
+"""
